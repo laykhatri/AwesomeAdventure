db83d548f6fc7b426f6649e3323d91ffeea8f715
Lay Khatri <laykhatri@hotmail.com>
2022-06-12 12:40:41 +0530

    FilePath Updated

--- src/app/_helper/monster.helper.ts
+++ src/app/_helper/monster.helper.ts
@@ -1,10 +1,10 @@
 export class monsters{
     monsterList= [
-        "../../../assets/monsters/monster1.png",
-        "../../../assets/monsters/monster2.png",
-        "../../../assets/monsters/monster3.png",
-        "../../../assets/monsters/monster4.png",
-        "../../../assets/monsters/monster5.png",
+        "./assets/monsters/monster1.png",
+        "./assets/monsters/monster2.png",
+        "./assets/monsters/monster3.png",
+        "./assets/monsters/monster4.png",
+        "./assets/monsters/monster5.png",
     ];
 
     getMonster(index: number){
